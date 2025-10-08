b45751cbaff1116a23d41c921ef98d64efea5942
stiven45-tech <olalerewealth2@gmail.com>
2025-10-08 11:59:37 +0100

    Create TodoList Component.jsx

---
+++ TodoList Component.jsx
@@ -0,0 +1,27 @@
+// src/components/TodoList.jsx
+import { useState } from 'react';
+
+export default function TodoList() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Learn React', completed: false },
+    { id: 2, text: 'Build a Todo App', completed: false },
+    { id: 3, text: 'Master Testing', completed: false }
+  ]);
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: Date.now(),
+      text,
+      completed: false
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(todos.map(todo =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    ));
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo =>
